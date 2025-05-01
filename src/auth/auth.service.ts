477885cdb1d2b3129e4f6a91d8720505725a6f9e
Aa <aa@example.com>
2025-05-01 18:40:16 +0300

    initial

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,20 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import { RegisterRequest } from './dto/register.dto';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private readonly prismaService: PrismaService) {}
+
+  async register(dto: RegisterRequest): Promise<void> {
+    const { name, email, password } = dto;
+
+    const existUser = await this.prismaService.user.findUnique({
+      where: { email },
+    });
+
+    if (existUser) {
+      throw new ConflictException('User already exist');
+    }
+  }
+}
