efdaf5089161d04eeaf3fe34d07ae3a573dcf3a1
Aa <aa@example.com>
2025-05-02 09:16:59 +0300

    initial

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,10 +1,29 @@
 import { ConflictException, Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import { RegisterRequest } from './dto/register.dto';
+import { hash } from 'argon2';
+import { ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly prismaService: PrismaService) {}
+  private readonly JWT_SECRET: string;
+  private readonly JWT_ACCESS_TOKEN_TTL: string;
+  private readonly JWT_REFRESH_TOKEN_TTL: string;
+
+  constructor(
+    private readonly prismaService: PrismaService,
+    private readonly configService: ConfigService,
+    private readonly jwtService: JwtService,
+  ) {
+    this.JWT_SECRET = configService.getOrThrow<string>('JWT_SECRET');
+    this.JWT_ACCESS_TOKEN_TTL = configService.getOrThrow<string>(
+      'JWT_ACCESS_TOKEN_TTL',
+    );
+    this.JWT_REFRESH_TOKEN_TTL = configService.getOrThrow<string>(
+      'JWT_REFRESH_TOKEN_TTL',
+    );
+  }
 
   async register(dto: RegisterRequest): Promise<any> {
     const { name, email, password } = dto;
@@ -18,7 +37,7 @@ export class AuthService {
     }
 
     const user = await this.prismaService.user.create({
-      data: { name, email, password },
+      data: { name, email, password: await hash(password) },
     });
 
     return user;
