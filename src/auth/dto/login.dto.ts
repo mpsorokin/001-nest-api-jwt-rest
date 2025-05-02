77910a62343600e62914b7111ecc96aac0549e14
Aa <aa@example.com>
2025-05-02 12:19:39 +0300

    swagger setup

--- src/auth/dto/login.dto.ts
+++ src/auth/dto/login.dto.ts
@@ -5,13 +5,24 @@ import {
   MaxLength,
   MinLength,
 } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class LoginRequest {
+  @ApiProperty({
+    description: 'user email',
+    example: 'user@mail.com',
+  })
   @IsString()
   @IsNotEmpty()
   @IsEmail()
   email: string;
 
+  @ApiProperty({
+    description: 'password',
+    example: 'password',
+    minLength: 6,
+    maxLength: 128,
+  })
   @IsString()
   @IsNotEmpty()
   @MinLength(6)
