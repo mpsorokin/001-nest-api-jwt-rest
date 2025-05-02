253cfd2b2e784cd9129f52db56b6ea6a2dba48c7
Aa <aa@example.com>
2025-05-02 15:39:13 +0300

    cleaned up

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,8 @@
-import {  Module } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from './prisma/prisma.module';
-import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -11,7 +10,6 @@ import { AuthModule } from './auth/auth.module';
       isGlobal: true,
     }),
     PrismaModule,
-    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
